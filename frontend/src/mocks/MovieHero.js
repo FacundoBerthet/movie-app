a3a08d57b153905edef6624af08604b7ae831b55
Facundo Berthet <f.berthet2001@gmail.com>
2026-03-17 20:08:45 -0300

    add mockHero

---
+++ frontend/src/mocks/MovieHero.js
@@ -0,0 +1,22 @@
+
+export const mockHero = {
+    id: 550,
+    title: "Fight Club",
+    overview: "Un oficinista insomne y un fabricante de jabón forman un club de lucha clandestino.",
+    backdrop_url: "https://image.tmdb.org/t/p/original/hZkgoQYus5vegHoetLkCJzb17zJ.jpg",
+    logo_url: "https://image.tmdb.org/t/p/original/8IgLvXhhr04vLKUZ62lYoyb9IAL.png",
+    genres: [{ id: 18, name: "Drama" }, { id: 53, name: "Thriller" }],
+    director: "David Fincher",
+    watch_providers: {
+        stream: [
+            { provider_id: 8,   name: "Netflix",        logo_url: "/t2yyOv40HZeVlLjYsCsPHnWLk4W.jpg" },
+            { provider_id: 384, name: "Max",             logo_url: "/Ajqyt5aNxNx9e1FkNxBiXHFPtF4.jpg" },
+            { provider_id: 337, name: "Disney+",         logo_url: "/97yvRBw1GzX7fXprcF80er19ot.jpg" },
+            { provider_id: 119, name: "Amazon Prime",    logo_url: "/68MNrwlkpF7WnmNPXLah69CR5cb.jpg" },
+            { provider_id: 350, name: "Apple TV+",       logo_url: "/6uhKBfmtzFqOcLousHwZuzcrScK.jpg" },
+            { provider_id: 283, name: "Crunchyroll",     logo_url: "/8Gt1iClBlzTeQs8WQm8UrCoIARo.jpg" },
+        ],
+        rent: [],
+        buy: []
+    }
+}
