ca361e1c88481f1a392445bbebeccca3ce109ef5
Facundo Berthet <f.berthet2001@gmail.com>
2026-03-18 14:29:19 -0300

    add movierows

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,18 +1,43 @@
 import { useEffect, useState } from 'react'
 import MovieGrid from '../components/MovieGrid'
-import { getUpcomingMovies } from '../services/movieService'
+import MovieRow from '../components/MovieRow'
+import Navbar from '../components/Navbar'
+import HeroBanner from '../components/HeroBanner'
+import { getUpcomingMovies, getHeroMovie } from '../services/movieService'
 
 export default function Home() {
+    const[hero, setHero] = useState(null)
     const [movies, setMovies] = useState([])
 
     useEffect(() => {
         getUpcomingMovies().then(data => setMovies(data.results))
+        getHeroMovie().then(data => setHero(data))
     }, [])
 
     return(
-        <div className="bg-zinc-950 min-h-screen">
-            <h2 className="text-white text-2xl font-bold px-6 pt-6">Próximos estrenos</h2>
-            <MovieGrid movies={movies} />
+        <div className='bg-sala'>
+            <Navbar />
+
+            <div className='hidden md:block'>    
+                {hero && <HeroBanner movie={hero}/>}
+            </div>
+
+            <div className="relative z-10 max-w-6xl mx-auto mt-0 md:-mt-32 pt-20 md:pt-0">
+                <MovieRow movies={movies} title={"En cartelera"} />
+            </div>
+
+            <div className="max-w-6xl mx-auto">
+                <MovieRow movies={movies} title={"Próximos estrenos"} />
+            </div>
+
+            <div className="max-w-6xl mx-auto">
+                <MovieRow movies={movies} title={"Populares esta semana"} />
+            </div>
+
+            <div className="max-w-6xl mx-auto">
+                <MovieRow movies={movies} title={"Mejor valoradas"} />
+            </div>
+
         </div>
     )
 }
