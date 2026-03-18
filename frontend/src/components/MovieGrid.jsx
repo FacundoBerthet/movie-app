beba24bf4de005094a1e6455d1ed80c8821c89c0
Facundo Berthet <f.berthet2001@gmail.com>
2026-03-17 23:46:58 -0300

    remove padding

--- frontend/src/components/MovieGrid.jsx
+++ frontend/src/components/MovieGrid.jsx
@@ -4,7 +4,7 @@ export default function MovieGrid({ movies }){
     
     return (
         <div 
-            className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-3 p-6">
+            className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-3">
             {movies.map(movie => (
                 <MovieCard key={movie.id} movie={movie} />
             ))}   
