7341482b55f1a7c87da2d49204505ec759636277
Facundo Berthet <f.berthet2001@gmail.com>
2026-03-18 14:51:41 -0300

    add movie detail mock

---
+++ frontend/src/mocks/movieDetail.js
@@ -0,0 +1,337 @@
+export const movieDetailMock = {
+  "id": 550,
+  "title": "El club de la lucha",
+  "overview": "Un hombre consumido por el insomnio y una vida sin sentido conoce a Tyler Durden, un carismático anarquista que lo arrastra a un club secreto donde el dolor despierta algo dormido. Lo que comienza como liberación pronto se convierte en una guerra contra el sistema…",
+  "release_date": "1999-10-15",
+  "poster_url": "https://image.tmdb.org/t/p/w500/sgTAWJFaB2kBvdQxRGabYFiQqEK.jpg",
+  "tagline": "Caos, travesuras y jabón",
+  "runtime": 139,
+  "backdrop_url": "https://image.tmdb.org/t/p/original/xRyINp9KfMLVjRiO5nCsoRDdvvF.jpg",
+  "genres": [
+    {
+      "id": 18,
+      "name": "Drama"
+    },
+    {
+      "id": 53,
+      "name": "Suspense"
+    }
+  ],
+  "cast": [
+    {
+      "id": 819,
+      "name": "Edward Norton",
+      "character": "Narrator",
+      "profile_url": "https://image.tmdb.org/t/p/w185/8nytsqL59SFJTVYVrN72k6qkGgJ.jpg"
+    },
+    {
+      "id": 287,
+      "name": "Brad Pitt",
+      "character": "Tyler Durden",
+      "profile_url": "https://image.tmdb.org/t/p/w185/cckcYc2v0yh1tc9QjRelptcOBko.jpg"
+    },
+    {
+      "id": 1283,
+      "name": "Helena Bonham Carter",
+      "character": "Marla Singer",
+      "profile_url": "https://image.tmdb.org/t/p/w185/hJMbNSPJ2PCahsP3rNEU39C8GWU.jpg"
+    },
+    {
+      "id": 7470,
+      "name": "Meat Loaf",
+      "character": "Robert Paulson",
+      "profile_url": "https://image.tmdb.org/t/p/w185/7gKLR1u46OB8WJ6m06LemNBCMx6.jpg"
+    },
+    {
+      "id": 7499,
+      "name": "Jared Leto",
+      "character": "Angel Face",
+      "profile_url": "https://image.tmdb.org/t/p/w185/ca3x0OfIKbJppZh8S1Alx3GfUZO.jpg"
+    },
+    {
+      "id": 7471,
+      "name": "Zach Grenier",
+      "character": "Richard Chesler (Regional Manager)",
+      "profile_url": "https://image.tmdb.org/t/p/w185/fSyQKZO39sUsqY283GXiScOg3Hi.jpg"
+    },
+    {
+      "id": 7497,
+      "name": "Holt McCallany",
+      "character": "The Mechanic",
+      "profile_url": "https://image.tmdb.org/t/p/w185/iRo9YUNMwZg4UCq7dapo0HydDmI.jpg"
+    },
+    {
+      "id": 7498,
+      "name": "Eion Bailey",
+      "character": "Ricky",
+      "profile_url": "https://image.tmdb.org/t/p/w185/3DW13W47cKk4LQZwS4EvRaNBoVu.jpg"
+    },
+    {
+      "id": 7472,
+      "name": "Richmond Arquette",
+      "character": "Intern at Hospital",
+      "profile_url": "https://image.tmdb.org/t/p/w185/7byGiVac0GjBSVD1h6ylZlVXZK6.jpg"
+    },
+    {
+      "id": 7219,
+      "name": "David Andrews",
+      "character": "Thomas at Remaining Men Together",
+      "profile_url": "https://image.tmdb.org/t/p/w185/36LEerIIN7gpG52mM3Ier7YWDbh.jpg"
+    },
+    {
+      "id": 1129738,
+      "name": "George Maguire",
+      "character": "Group Leader at Remaining Men Together",
+      "profile_url": "https://image.tmdb.org/t/p/w185/pyKzJNkpjCA96ItVq93x1Y2YfYx.jpg"
+    },
+    {
+      "id": 1317693,
+      "name": "Eugenie Bondurant",
+      "character": "Weeping Woman - Onward and Upward",
+      "profile_url": "https://image.tmdb.org/t/p/w185/9ULAELEKNha7VCJhRWoer58NcJe.jpg"
+    },
+    {
+      "id": 68277,
+      "name": "Christina Cabot",
+      "character": "Group Leader - Partners in Positivity",
+      "profile_url": "https://image.tmdb.org/t/p/w185/h1vwbOfITSvDvDq8E9MVvWqMYSr.jpg"
+    },
+    {
+      "id": 202080,
+      "name": "Sydney 'Big Dawg' Colston",
+      "character": "Speaker - Free and Clear",
+      "profile_url": "https://image.tmdb.org/t/p/w185/tdzVh7pstj6rcppqbBOX0KA7mX9.jpg"
+    },
+    {
+      "id": 7473,
+      "name": "Rachel Singer",
+      "character": "Chloe",
+      "profile_url": "https://image.tmdb.org/t/p/w185/o4Hzvy1VRa3IILMrI45Ia08pstK.jpg"
+    },
+    {
+      "id": 1172435,
+      "name": "Christie Cronenweth",
+      "character": "Airline Check-In Attendant",
+      "profile_url": "https://image.tmdb.org/t/p/w185/m9S13jmuBO2VeY0ZspSS7DlAEU.jpg"
+    },
+    {
+      "id": 956719,
+      "name": "Tim DeZarn",
+      "character": "Federated Motor Co. - Inspector Bird",
+      "profile_url": "https://image.tmdb.org/t/p/w185/tduRqomAU3LARfuDa2OMKFAd1rR.jpg"
+    },
+    {
+      "id": 59285,
+      "name": "Ezra Buzzington",
+      "character": "Federated Motor Co. - Inspector Dent",
+      "profile_url": "https://image.tmdb.org/t/p/w185/vnkdumEom4lVAqdh7Odsf2nZpjB.jpg"
+    },
+    {
+      "id": 1705289,
+      "name": "Dierdre Downing-Jackson",
+      "character": "Business Woman on Plane",
+      "profile_url": "https://image.tmdb.org/t/p/w185/81NKNmWmruKfvN6ZTAJmUVgz2yR.jpg"
+    },
+    {
+      "id": 17449,
+      "name": "Bob Stephenson",
+      "character": "Airport Security Officer",
+      "profile_url": "https://image.tmdb.org/t/p/w185/6RICLz0vofoGkI339Pk5sVn1657.jpg"
+    }
+  ],
+  "watch_providers": {
+    "stream": [
+      {
+        "id": 119,
+        "name": "Amazon Prime Video",
+        "logo_url": "https://image.tmdb.org/t/p/w92/pvske1MyAoymrs5bguRfVqYiM9a.jpg"
+      },
+      {
+        "id": 337,
+        "name": "Disney Plus",
+        "logo_url": "https://image.tmdb.org/t/p/w92/97yvRBw1GzX7fXprcF80er19ot.jpg"
+      },
+      {
+        "id": 339,
+        "name": "MovistarTV",
+        "logo_url": "https://image.tmdb.org/t/p/w92/tRNA2CRgA4XHvd7Mx9dH3sFtDVb.jpg"
+      }
+    ],
+    "rent": [
+      {
+        "id": 3,
+        "name": "Google Play Movies",
+        "logo_url": "https://image.tmdb.org/t/p/w92/8z7rC8uIDaTM91X0ZfkRf04ydj2.jpg"
+      }
+    ],
+    "buy": [
+      {
+        "id": 3,
+        "name": "Google Play Movies",
+        "logo_url": "https://image.tmdb.org/t/p/w92/8z7rC8uIDaTM91X0ZfkRf04ydj2.jpg"
+      }
+    ]
+  },
+  "recommendations": [
+    {
+      "id": 146015,
+      "title": "El Doble",
+      "overview": "Adaptación contemporánea de la novela de Dostoievski sobre un burócrata que empieza a perder la cabeza cuando un doble exacto a él aparece trabajando en su misma oficina y parece tener intenciones de suplantarle.",
+      "year": "2014",
+      "rating": 6.509,
+      "poster_url": "https://image.tmdb.org/t/p/w500/7kNcpmP1Pe9fWLKEbEOX5GEWueC.jpg"
+    },
+    {
+      "id": 16229,
+      "title": "Prozac Nation",
+      "overview": "Durante el primer curso en la prestigiosa universidad de Harward, una joven (Christina Ricci) tiene que enfrentarse a una profunda depresión.",
+      "year": "2003",
+      "rating": 6.32,
+      "poster_url": "https://image.tmdb.org/t/p/w500/xOcVCtGFOFxwSMJmEKer66BEfUn.jpg"
+    },
+    {
+      "id": 22473,
+      "title": "El libro mágico",
+      "overview": "Al terminar sus estudios universitarios, Zach Riley (Aaron Eckhart) empieza a trabajar en un psiquiátrico del que guarda un ingrato recuerdo: allí estuvo internado hasta su muerte su padre (Nick Nolte), que era escritor. Enseguida traba relación con un enfermo (Ian McKellen) y con una conocida de la infancia (Brittany Murphy) que sienten una gran devoción por \"Siempre jamás\", un libro del padre de Zach.",
+      "year": "2005",
+      "rating": 6.386,
+      "poster_url": "https://image.tmdb.org/t/p/w500/orV3Xc6yJXGp1P9xN1BOeJryrWM.jpg"
+    },
+    {
+      "id": 3558,
+      "title": "Inocencia interrumpida",
+      "overview": "Año 1967. Susanna Kaysen, de 17 años, es como la mayoría de las chicas americanas de su edad: está confusa, se siente insegura y lucha por entender el mundo que la rodea. Su psiquiatra decide ingresarla en el Hospital Claymoore, diagnosticándole un trastorno de la personalidad, que se manifiesta a través de una gran inseguridad respecto a la propia imagen, a los objetivos a largo plazo y a los amigos. En el centro Susanna conoce a Lisa, Georgina, Polly y Janet, un grupo de chicas inadaptadas con las que entabla una estrecha amistad y que, además, le muestran el camino para encontrarse a sí misma.",
+      "year": "1999",
+      "rating": 7.565,
+      "poster_url": "https://image.tmdb.org/t/p/w500/kKBHjBY9RLPyIJaycEdduPj0cjB.jpg"
+    },
+    {
+      "id": 7299,
+      "title": "Equilibrium",
+      "overview": "En el mundo del futuro, un régimen estricto ha eliminado las guerras al haber superado las emociones. Los libros, el arte y la música están totalmente prohibidos, y los sentimientos se consideran un crimen castigado con la pena de muerte. El clérigo John Preston (Christian Bale) es un agente de élite del gobierno, responsable de combatir aquellos que osen desafiar las reglas. Cuando pierde una dosis de \"Prozium\", una droga que altera la mente para ocultar sus emociones, toda su escala de valores cambiará de manera súbita.",
+      "year": "2002",
+      "rating": 7.045,
+      "poster_url": "https://image.tmdb.org/t/p/w500/yQuovle5Ov6q69XSdTNdH52eANc.jpg"
+    },
+    {
+      "id": 1213,
+      "title": "El talento de Mr. Ripley",
+      "overview": "Ser joven y despreocupado en medio de un paisaje idílico de la Italia bañada por el sol de finales de los 50: esa es la dolce vita que Tom Ripley (Matt Damon) anhela, y que Dickie Greeleaf (Jude Law) disfruta. Cuando el padre de Dickie, un adinerado constructor de barcos, pide a Tom que traiga de vuelta a casa, a América, a su hijo, éste y su bella y expatriada compañera, Marge Sherwood (Gwyneth Paltrow), nunca hubieran sospechado a qué peligrosos extremos llegaría Ripley con tal de convertir el estilo de vida de la pareja en el suyo propio. Al fin y al cabo, es mejor ser una falsa persona importante que un don nadie auténtico.",
+      "year": "1999",
+      "rating": 7.199,
+      "poster_url": "https://image.tmdb.org/t/p/w500/idVOOIQGbf22TF58amdYvHBL9Ua.jpg"
+    },
+    {
+      "id": 604079,
+      "title": "La larga marcha",
+      "overview": "En un futuro distópico, cien adolescentes participan en una brutal competición conocida como \"La larga marcha\", donde deben caminar sin descanso: si se detienen o reducen la velocidad de la marcha, mueren. Solo uno sobrevivirá. Adaptación cinematográfica de la novela de Stephen King.",
+      "year": "2025",
+      "rating": 6.921,
+      "poster_url": "https://image.tmdb.org/t/p/w500/vH7WPEgHxy1JLKsjBgSfY7NIf9W.jpg"
+    },
+    {
+      "id": 22784,
+      "title": "El tren de Bertha",
+      "overview": "\"Boxcar\" Bertha Thompson es una joven de la era de la Gran Depresión que al perder a su padre se une a un controvertido líder sindical llamado Bill Shelley. Acusados de comunistas por un grupo de conservadores y perseguidos por una corrupta compañía de ferrocarriles que busca venganza contra Shelley, la vida de Bertha se convierte en una permanente huida por el mundo del crimen y un emocionante capítulo de la historia americana.",
+      "year": "1972",
+      "rating": 5.8,
+      "poster_url": "https://image.tmdb.org/t/p/w500/A1yZRKbmtmFNb4X9R3iCIWofvnv.jpg"
+    },
+    {
+      "id": 3594,
+      "title": "El número 23",
+      "overview": "Narra la historia de un hombre obsesionado con un libro, cuyo relato parece describir detalles de su vida íntima. El hombre comenzará a sentirse amenazado y desarrolla una paranoia por un número que se repite una y otra vez en todo el libro: el 23.",
+      "year": "2007",
+      "rating": 6.411,
+      "poster_url": "https://image.tmdb.org/t/p/w500/h0T3FmHI7PFhUseoySCBPFjxP0O.jpg"
+    },
+    {
+      "id": 8066,
+      "title": "Tránsito",
+      "overview": "Henry (Ryan Gosling), un joven mentalmente perturbado, le anuncia a Sam Forster, su psiquiatra (Ewan McGregor), que piensa suicidarse en un plazo de tres días, cosa que el psiquiatra intentará evitar. Por su parte, Henry se dedica a enturbiar la relación de Sam con su novia Lila (Naomi Watts), pintora y antigua paciente suya. Poco a poco, la sólida y arraigada racionalidad de Sam empieza a tambalearse. Sometido a situaciones cada vez más surrealistas, llega un momento en que ya no puede distinguir entre realidad y ficción, y su yo y el de Henry se confunden.",
+      "year": "2005",
+      "rating": 6.604,
+      "poster_url": "https://image.tmdb.org/t/p/w500/mvNAb4PSoRAyN0RkRnT1y0qVwue.jpg"
+    },
+    {
+      "id": 38731,
+      "title": "Una mujer descasada",
+      "overview": "Erica es una mujer feliz tras sus dieciocho años de matrimonio. La vida para esta mujer resulta perfecta. Pero todo cambia cuando un día su marido le confiesa que se ha enamorado de otra y que quiere irse a vivir con ella. A partir de este momento, Erica tendrá que tratar de sobreponerse completamente sola a su nueva situación.",
+      "year": "1978",
+      "rating": 6.709,
+      "poster_url": "https://image.tmdb.org/t/p/w500/wuQ3dCSSknKFupEyTXsAYrocFpw.jpg"
+    },
+    {
+      "id": 2742,
+      "title": "El almuerzo desnudo",
+      "overview": "Después del trágico accidente sufrido por su mujer, un escritor que cae en la adicción a ciertas drogas, empieza a sufrir horribles alucinaciones que lo transportan a un mundo de pesadilla absolutamente kafkiano.",
+      "year": "1991",
+      "rating": 7.008,
+      "poster_url": "https://image.tmdb.org/t/p/w500/HzpDsHbgJWqBlmN5bpnHAhCDMF.jpg"
+    },
+    {
+      "id": 10333,
+      "title": "El príncipe de las mareas",
+      "overview": "Tras una infancia traumática que no ha conseguido olvidar, Tom Wingo (Nick Nolte), un desilusionado y algo rudo entrenador deportivo sureño, se ve obligado a revivir su pasado cuando viaja a Nueva York a petición de la doctora Susan Lowenstein (Barbra Streisand), la sofisticada psiquiatra de la alta sociedad de Manhattan que atiende a la hermana de Tom después de varios intentos de suicidio.",
+      "year": "1991",
+      "rating": 6.604,
+      "poster_url": "https://image.tmdb.org/t/p/w500/dxCYNnkr8h5qbm4gZ7yXa9bX7JP.jpg"
+    },
+    {
+      "id": 9445,
+      "title": "Verano de corrupción",
+      "overview": "Un día de 1984, Todd Bowden, un brillante chico de secundaria fascinado por la historia del nazismo, tropieza casualmente con un anciano cuyo aspecto se asemeja al de Kurt Dussander, un buscado criminal de guerra nazi. Un mes después, Todd decide llamar a su puerta.",
+      "year": "1998",
+      "rating": 6.533,
+      "poster_url": "https://image.tmdb.org/t/p/w500/sLXmsBCBRg29ap6hiXFs8zvT2Rt.jpg"
+    },
+    {
+      "id": 9314,
+      "title": "1984",
+      "overview": "El futuro, año 1984. Winston Smith (John Hurt) soporta una abyecta existencia bajo la continua vigilancia de las autoridades en la Oceanía totalitaria. No obstante, su vida se convierte en una pesadilla cuando prueba el amor prohibido y comete el crimen de pensar libremente.",
+      "year": "1984",
+      "rating": 6.786,
+      "poster_url": "https://image.tmdb.org/t/p/w500/jVRtxHwDkiKDKCinh9xy7lrg9go.jpg"
+    },
+    {
+      "id": 340666,
+      "title": "Animales nocturnos",
+      "overview": "Susan Morrow es una galerista afincada en Los Ángeles que comparte una vida llena de lujos, aunque vacía, con Hutton Morrow, su segundo marido. Un día, Susan recibe una novela escrita por su exmarido Edward Sheffiel, del que lleva años sin tener noticias. En una nota le pide que lea la novela inédita y que se ponga en contacto con él, pues se quedará unos días en la ciudad. Es de noche. Susan, sola en la cama, empieza a leer, y es la historia de un matrimonio con una hija que conduciendo por Texas una noche, son perseguidos por otro vehículo...",
+      "year": "2016",
+      "rating": 7.412,
+      "poster_url": "https://image.tmdb.org/t/p/w500/5T2t0m8VHofFxk2LhMmDYhMod6l.jpg"
+    },
+    {
+      "id": 38,
+      "title": "¡Olvídate de mí!",
+      "overview": "Una mujer usa los servicios de una empresa para borrar de su memoria todo recuerdo de su ex pareja. Ofendido, el hombre intenta hacer lo mismo que ella, pero el proceso no sucede según lo esperado y el protagonista debe atravesar la gigantesca marea de recuerdos de su propio cerebro para recomponer las cosas.",
+      "year": "2004",
+      "rating": 8.091,
+      "poster_url": "https://image.tmdb.org/t/p/w500/2bHEXzzccUsBNOS6rn9uvi3vAGW.jpg"
+    },
+    {
+      "id": 347629,
+      "title": "Deber cumplido",
+      "overview": "Tres soldados estadounidenses regresan de Irak tratando de reintegrarse con sus familias, y adaptándose a la vida civil mientras luchan por olvidar sus duros recuerdos de la guerra.",
+      "year": "2017",
+      "rating": 6.562,
+      "poster_url": "https://image.tmdb.org/t/p/w500/1sMGs4XnQxf1XFv4QriirKxIzX1.jpg"
+    },
+    {
+      "id": 37094,
+      "title": "Un día de furia",
+      "overview": "En Los Ángeles, durante una jornada especialmente agobiante a causa del calor y del colapso del tráfico, de repente, un ciudadano normal se rebela de manera violenta y destructiva contra todo lo que lo rodea. Bill Foster (Michael Douglas) no es más que un hombre corriente que supera como puede las frustraciones de cada día y que lo único que quiere es regresar a casa. Un oficial del departamento de policía (Robert Duvall) intentará poner fin a su violencia y agresividad.",
+      "year": "1993",
+      "rating": 7.392,
+      "poster_url": "https://image.tmdb.org/t/p/w500/kFBDnvgkSsVrBAjej1RriGOB8BV.jpg"
+    },
+    {
+      "id": 426249,
+      "title": "Señores del caos",
+      "overview": "La historia de un joven que quiere darle un nuevo giro a la escena del black metal en Oslo, Noruega, proyecto que trae fatales consecuencias para todos los involucrados. Biopic de la banda de black metal noruego Mayhem.",
+      "year": "2018",
+      "rating": 6.79,
+      "poster_url": "https://image.tmdb.org/t/p/w500/5jDShbwEsILXa2AAmAbOLxyNwyq.jpg"
+    }
+  ]
+}
