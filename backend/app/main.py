d17e793a181c12d6c62230da1fa27981ecd63a55
Facundo Berthet <f.berthet2001@gmail.com>
2026-03-20 19:31:21 -0300

    add CORS middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.config import get_settings
 
 from app.routers import movies
@@ -8,6 +9,14 @@ settings = get_settings()
 app = FastAPI(
     title=settings.app_name,
     description="API Backend para busqueda de peliculas y proximos lanzamientos"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173", # front local desarrollo
+                   "http://localhost:4173"], # preview build
+    allow_methods=["GET"],                    # solo GET 
+    allow_headers=["*"],
 )
 
 app.include_router(movies.router)
