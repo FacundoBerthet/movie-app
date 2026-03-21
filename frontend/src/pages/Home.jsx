d74e97a4469e37f166c7d4ef29736def51438f49
Facundo Berthet <f.berthet2001@gmail.com>
2026-03-21 13:07:33 -0300

    add  components to fetch and display multiple movie categories

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,16 +3,32 @@ import MovieGrid from '../components/MovieGrid'
 import MovieRow from '../components/MovieRow'
 import Navbar from '../components/Navbar'
 import HeroBanner from '../components/HeroBanner'
-import { getUpcomingMovies, getHeroMovie } from '../services/movieService'
+import { getUpcomingMovies, getHeroMovie, 
+        getNowPlaying, getTrending, getTopRated} from '../services/movieService'
 
 export default function Home() {
-    const[hero, setHero] = useState(null)
-    const [movies, setMovies] = useState([])
+    const [hero, setHero] = useState(null)
+    const [nowPlaying, setNowPlaying] = useState([])
+    const [upcoming, setUpcoming] = useState([])
+    const [trending, setTrending] = useState([])
+    const [topRated, setTopRated] = useState([])
 
     useEffect(() => {
-        getUpcomingMovies().then(data => setMovies(data.list_of_movies))
-        getHeroMovie().then(data => setHero(data))
+        Promise.all([
+            getHeroMovie(),
+            getNowPlaying(),
+            getUpcomingMovies(),
+            getTrending(),
+            getTopRated(),
+        ]).then(([heroData, nowData, upcomingData, trendingData, topData]) => {
+            setHero(heroData)
+            setNowPlaying(nowData.list_of_movies)
+            setUpcoming(upcomingData.list_of_movies)
+            setTrending(trendingData.list_of_movies)
+            setTopRated(topData.list_of_movies)
+        })
     }, [])
+
 
     return(
         <div className='bg-sala'>
@@ -21,20 +37,20 @@ export default function Home() {
                 {hero && <HeroBanner movie={hero}/>}
             </div>
 
-            <div className="relative z-10 max-w-6xl mx-auto mt-0 md:-mt-32 pt-20 md:pt-0">
-                <MovieRow movies={movies} title={"En cartelera"} />
+            <div className="relative z-10 max-w-6xl mx-auto mt-0 md:-mt-44 pt-20 md:pt-0">
+                <MovieRow movies={nowPlaying} title={"En cartelera"} />
             </div>
 
             <div className="max-w-6xl mx-auto">
-                <MovieRow movies={movies} title={"Próximos estrenos"} />
+                <MovieRow movies={upcoming} title={"Próximos estrenos"} />
             </div>
 
             <div className="max-w-6xl mx-auto">
-                <MovieRow movies={movies} title={"Populares esta semana"} />
+                <MovieRow movies={trending} title={"Populares esta semana"} />
             </div>
 
             <div className="max-w-6xl mx-auto">
-                <MovieRow movies={movies} title={"Mejor valoradas"} />
+                <MovieRow movies={topRated} title={"Mejor valoradas"} />
             </div>
 
         </div>
