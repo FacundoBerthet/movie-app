116e495fc74862d0388e1cc2bb1dcf18ab760821
Facundo Berthet <f.berthet2001@gmail.com>
2026-03-21 13:03:54 -0300

    disable mock

--- frontend/src/services/movieService.js
+++ frontend/src/services/movieService.js
@@ -2,26 +2,44 @@ import { upcomingMoviesMock } from "../mocks/movies"
 import {mockHero}  from "../mocks/MovieHero"
 import { movieDetailMock } from "../mocks/movieDetail"
 
-const USE_MOCK = true
+const USE_MOCK = false
 const BASE_URL = "http://localhost:8000"
 
 export async function getUpcomingMovies({ page = 1 } = {}) {
-  if (USE_MOCK) return upcomingMoviesMock
-  const res = await fetch(`${BASE_URL}/movies/upcoming?page=${page}`)
-  if (!res.ok) throw new Error("Error al obtener estrenos")
-  return res.json()
+    if (USE_MOCK) return upcomingMoviesMock
+    const res = await fetch(`${BASE_URL}/movies/upcoming?page=${page}`)
+    if (!res.ok) throw new Error("Error al obtener estrenos")
+    return res.json()
 }
 
 export async function getHeroMovie() {
     if (USE_MOCK) return mockHero
     const res = await fetch(`${BASE_URL}/movies/hero`)
-    if (!res.ok) throw new Error("Error al obtener película destacada")
+    if (!res.ok) throw new Error("Error al obtener hero")
     return res.json()
 }
 
 export async function getMovieDetail(id) {
     if (USE_MOCK) return movieDetailMock
     const res = await fetch(`${BASE_URL}/movies/${id}`)
-    if (!res.ok) throw new Error("Error al obtener detalle de película")
+    if (!res.ok) throw new Error("Error al obtener detalle")
+    return res.json()
+}
+
+export async function getNowPlaying({ page = 1 } = {}) {
+    const res = await fetch(`${BASE_URL}/movies/now_playing?page=${page}`)
+    if (!res.ok) throw new Error("Error al obtener en cartelera")
+    return res.json()
+}
+
+export async function getTrending() {
+    const res = await fetch(`${BASE_URL}/movies/trending`)
+    if (!res.ok) throw new Error("Error al obtener tendencias")
+    return res.json()
+}
+
+export async function getTopRated({ page = 1 } = {}) {
+    const res = await fetch(`${BASE_URL}/movies/top_rated?page=${page}`)
+    if (!res.ok) throw new Error("Error al obtener mejor valoradas")
     return res.json()
 }
